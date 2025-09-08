8c313e4ca10cc93f30a1f611bc4305e1418d8fa3
Yinkai Wang <yinkai.wang@tufts.edu>
2025-09-07 20:02:52 -0400

    Update preprocess_rank.py

--- data/msgym/raw/preprocess_rank.py
+++ data/msgym/raw/preprocess_rank.py
@@ -18,21 +18,20 @@ sca_list = pickle.load(open('./ranks_total_1740431736104.pkl', 'rb'))
 correct_count = 0
 total_count = 0
 sca_dict = {}
-k = 30
+k = 20
 for ele in sca_list:
-    query = Chem.MolFromSmiles(ele[0]) # Query molecule
-    if query is None:
-        continue
-    formula = Chem.rdMolDescriptors.CalcMolFormula(query)
-    # predictions = [get_sca_smile(mol_dict.get(i)) for i in ele[4][:k]]  # First prediction
+    # query = mol_dict.get(ele[0])  # Query molecule
+    query = ele[0]  # Query molecule
+    # predictions = [mol_dict.get(i) for i in ele[4][:k]]  # First prediction
     predictions = ele[4][:k]  # First prediction
     for prediction in predictions:
-        if formula not in sca_dict.keys():
-            sca_dict[formula] = {}
-        if prediction not in sca_dict[formula].keys():
-            sca_dict[formula][prediction] = 1
+        if query not in sca_dict.keys():
+            sca_dict[query] = {}
+        if prediction not in sca_dict[query].keys():
+            sca_dict[query][prediction] = 1
         else:
-            sca_dict[formula][prediction] += 1
+            sca_dict[query][prediction] += 1
+
 # for i in range(len(sca_list)):
 #     if get_sca_smile(sca_list[i][0]) in [get_sca_smile(p) for p in sca_list[i][4]]:
 #         print(i)
@@ -55,21 +54,8 @@ for i in sca_dict.keys():
 #     if get_sca_smile(mol_dict.get(i[0])) == get_sca_smile(list(sca_dict[mol_dict.get(i[0])].keys())[0]):
 #         count +=1
 result_dict = {}
-# result_dict = {}
-# for i in sca_list:
-#     formula = Chem.rdMolDescriptors.CalcMolFormula(mol_dict.get(i[0]))
-#     result_dict[i[1]] = get_sca_smile(list(sca_dict[formula].keys())[0])
-# pickle.dump(result_dict, open('./ranks_canopus_pred.pkl', 'wb'))
-# print(len(sca_dict.keys()), sca_dict.keys())
 count = 0
 for i in sca_list:
-    formula = Chem.rdMolDescriptors.CalcMolFormula(Chem.MolFromSmiles(i[0]))
-    if get_sca_smile(i[0]) == get_sca_smile(list(sca_dict[formula].keys())[0]):
-        count +=1
-print(count/len(sca_list))
-for i in sca_list:
-    formula = Chem.rdMolDescriptors.CalcMolFormula(Chem.MolFromSmiles(i[0]))
-    result_dict[i[1]] = get_sca_smile(list(sca_dict[formula].keys())[0])
+    result_dict[i[1]] = get_sca_smile(list(sca_dict[i[0]].keys())[0])
 
 pickle.dump(result_dict, open('./ranks_msgym_pred.pkl', 'wb'))
-
